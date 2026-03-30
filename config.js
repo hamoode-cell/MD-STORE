8438ae70432da00d6be39368aa1fdfed6a4545a9
hamoode-cell <hamoodetd2@gmail.com>
2026-03-31 00:12:47 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
-// إعدادات المتجر المركزية - MD STORE
-const shopSettings = {
-    walletNumber: "2bc3b1618430a2facee4f0267f96e530", // ضع رقم محفظتك هنا بين علامتي التنصيص
-    exchangeRate: 15000         // ضع سعر الصرف هنا (بدون علامات تنصيص)
+var shopSettings = {
+    walletNumber: "2bc3b1618430a2facee4f0267f96e530", 
+    exchangeRate: 15000
 };
